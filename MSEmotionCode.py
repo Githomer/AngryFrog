b834f66f53496caf7b5fc1044c700f74676570fe
chung1204 <honestman5@naver.com>
2017-05-15 14:54:35 +0900

    Update MSEmotionCode.py
    change python code and analysis photo using emotion api

--- MSEmotionCode.py
+++ MSEmotionCode.py
@@ -10,7 +10,7 @@ params = urllib.parse.urlencode({
 })
 
 # Replace the example URL below with the URL of the image you want to analyze.
-body = "{ 'url': 'http://example.com/picture.jpg' }"// ÀÌ°Å¸¦ À¯¾Ë¿¤ ¹Ù²Û´Ù
+body = "{ 'url': 'https://upload.wikimedia.org/wikipedia/commons/thumb/3/3f/Bae_Suzy_at_%22Uncontrollably_Fond%22_press_conference%2C_4_July_2016_05.jpg/250px-Bae_Suzy_at_%22Uncontrollably_Fond%22_press_conference%2C_4_July_2016_05.jpg' }"
 
 try:
     conn = http.client.HTTPSConnection('westus.api.cognitive.microsoft.com')
